87e016279ef12fbb53fa14f05b36d53bed643a39
NeoGlanding <neoglanding@protonmail.com>
2022-01-12 21:38:53 +0700

    auto commit without message

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>My friends are Jonas</h1>
+    <ul>
+        <li></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 2,
+                    name: 'Stephan Lichtsteiner'
+                },
+                {
+                    id: 2,
+                    name: 'Martin Caceres'
+                }
+            ]
+        }
+    }
+}
+</script>
